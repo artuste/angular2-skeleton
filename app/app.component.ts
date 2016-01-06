fa2463e647b541009ad56429af2340134e511735
artuste <artuste@gmail.com>
2016-01-06 16:11:08 +0100

    Routing added.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,37 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {UsersComponent} from './users/users.component';
+import {UserDetailComponent} from './users/user-detail.component';
 import {DashboardComponent} from './dashboard/dashboard'
 
 @Component({
     selector: 'app',
     template: `
-      <h1>{{ title }}</h1>
-      <dashboard></dashboard>
-      <users></users>`,
-    directives: [UsersComponent, DashboardComponent]
+        <div class="container">
+        <h1>{{ title }}</h1>
+
+        <ul class="nav nav-pills">
+          <li role="presentation"><a [routerLink]="['Dashboard']">Dashboard</a></li>
+          <li role="presentation"><a [routerLink]="['Users']">Users</a></li>
+        </ul>
+
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <router-outlet></router-outlet>
+          </div>
+        </div>
+        </div>`,
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+    {path: '/users', name: 'Users', component: UsersComponent},
+    {path: '/users/:id', name: 'UserDetail', component: UserDetailComponent}
+])
 export class AppComponent {
     public title = 'Angular2 Skeleton App';
+
+    ngOnInit() {
+        console.log('AppComponent onInit');
+    }
 }
